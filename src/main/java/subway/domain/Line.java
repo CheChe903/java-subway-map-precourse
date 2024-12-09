de4b857f96ad491d028bf46e68b54354a3247896
CheChe903 <cheche903@naver.com>
2024-12-09 20:06:50 +0900

    feat(Line): Line에 역 리스트 추가
    해당 객체에 역 리스트를 관리합니다.
    이는 해당 라인의 역 리스트입니다.
    이를 추가하기 위해 addStation 메서드도 추가합니다.

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,15 +1,20 @@
 package subway.domain;
 
+import java.util.List;
+
 public class Line {
     private String name;
+    private List<Station> stations;
 
     public Line(String name) {
         this.name = name;
     }
 
+    public void addStation(Station station) {
+        stations.add(station);
+    }
+
     public String getName() {
         return name;
     }
-
-    // 추가 기능 구현
 }
