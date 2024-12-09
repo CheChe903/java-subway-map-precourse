e43f812caed23170600cad741700693e67f28cd8
CheChe903 <cheche903@naver.com>
2024-12-09 20:08:58 +0900

    feat: 실행을 위한 초기 구현
    실행을 위한 기본 컨트롤러와 뷰를 구현합니다.
    또한 컨트롤러에 요구사항에 적힌 기본 역과 노선을 추가하는 메서드도 추가합니다.

---
+++ src/main/java/subway/controller/SubwayController.java
@@ -0,0 +1,62 @@
+package subway.controller;
+
+import subway.domain.Line;
+import subway.domain.LineRepository;
+import subway.domain.Station;
+import subway.domain.StationRepository;
+import subway.view.InputView;
+import subway.view.OutputView;
+
+public class SubwayController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+
+    public SubwayController(final InputView inputView, final OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        initializeSubway();
+    }
+
+    private void initializeSubway() {
+        String[] stations = {"교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역"};
+        for (String station : stations) {
+            StationRepository.addStation(new Station(station));
+        }
+
+        String[] lines = {"2호선", "3호선", "신분당선"};
+
+        for (String line : lines) {
+            LineRepository.addLine(new Line(line));
+        }
+        initialLineTwo();
+        initialLineThree();
+        initialLineShinbundangLine();
+    }
+
+    private void initialLineTwo() {
+        Line line = LineRepository.findLineByName("2호선");
+        line.addStation(StationRepository.findStationByName("교대역"));
+        line.addStation(StationRepository.findStationByName("강남역"));
+        line.addStation(StationRepository.findStationByName("역삼역"));
+    }
+
+    private void initialLineThree() {
+        Line line = LineRepository.findLineByName("3호선");
+        line.addStation(StationRepository.findStationByName("교대역"));
+        line.addStation(StationRepository.findStationByName("남부터미널역"));
+        line.addStation(StationRepository.findStationByName("양재역"));
+        line.addStation(StationRepository.findStationByName("매봉역"));
+    }
+
+    private void initialLineShinbundangLine() {
+        Line line = LineRepository.findLineByName("신분당선");
+        line.addStation(StationRepository.findStationByName("강남역"));
+        line.addStation(StationRepository.findStationByName("양재역"));
+        line.addStation(StationRepository.findStationByName("양재시민숲역"));
+    }
+}
