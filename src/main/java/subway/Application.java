dc252d52bd09d74bc8af38cb1a6357bbf8780b68
CheChe903 <cheche903@naver.com>
2024-12-09 20:29:57 +0900

    feat(Application): IOException

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,11 +1,12 @@
 package subway;
 
+import java.io.IOException;
 import subway.controller.SubwayController;
 import subway.view.InputView;
 import subway.view.OutputView;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
